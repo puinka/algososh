221afe08ea75dd2b98365bd9a36aaf34834b4128
Valeria <vozzheva@mail.ru>
2023-03-05 23:13:02 +0000

    feat: add linked list

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import style from "./list.module.css";
 
 export const ListPage: React.FC = () => {
   return (
     <SolutionLayout title="Связный список">
-
+      <form className={style.inputsContainer}>
+        <Input placeholder="Введите значение" />
+        <Button text="Добавить в head" />
+        <Button text="Добавить в tail" />
+        <Button text="Удалить из head" />
+        <Button text="Удалить из tail" />
+        <Input placeholder="Введите индекс" />
+        <Button text="Добавить по индексу" extraClass={style.indexAdd} />
+        <Button text="Удалить по индексу" extraClass={style.indexRemove} />
+      </form>
     </SolutionLayout>
   );
 };
